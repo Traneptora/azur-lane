2a66edf06dbc64aa6978644c889d0c554b469a1e
Leo Izen <leo.izen@gmail.com>
2020-03-26 18:21:36 -0400

    airstrike-reload: Generalize cooldown reduction

--- js/airstrike-reload.js
+++ js/airstrike-reload.js
@@ -1,4 +1,4 @@
-function get_airstrike_cooldown(plane1time, plane1count, plane2time, plane2count, plane3time, plane3count, reloadstat, reloadbuff, beacon){
+function get_airstrike_cooldown(plane1time, plane1count, plane2time, plane2count, plane3time, plane3count, reloadstat, reloadbuff, cooldown_reduction){
 	let plane_count = plane1count * 1.0 + plane2count * 1.0 + plane3count * 1.0;
 	if (plane_count <= 0){
 		return -1.0;
@@ -6,8 +6,8 @@ function get_airstrike_cooldown(plane1time, plane1count, plane2time, plane2count
 	let weighted_cooldown_average = (plane1time * plane1count + plane2time * plane2count + plane3time * plane3count ) / plane_count;
 	let adjusted_reload = (1 + reloadstat / 100.0 * (1 + reloadbuff / 100.0));
 	let cooldown = Math.pow(adjusted_reload, -0.5) * 3.111269837 * weighted_cooldown_average;
-	if (beacon){
-		cooldown = cooldown * 0.96;
+	if (cooldown_reduction){
+		cooldown = cooldown * (1.0 - cooldown_reduction);
 	}
 	cooldown = cooldown + 0.1;
 	if (cooldown > 0.0 && cooldown < 300.00){
@@ -27,7 +27,8 @@ function calculate_reload(){
 	let plane3time = $("#plane3cdtextfield").prop("value");
 	let plane3count = $("#plane3counttextfield").prop("value");
 	let beacon = $("#beaconbox").is(":checked");
-	let cooldown = get_airstrike_cooldown(plane1time, plane1count, plane2time, plane2count, plane3time, plane3count, reloadstat, reloadbuff, beacon);
+	let cooldown_reduction = beacon ? 0.00 : 0.04;
+	let cooldown = get_airstrike_cooldown(plane1time, plane1count, plane2time, plane2count, plane3time, plane3count, reloadstat, reloadbuff, cooldown_reduction);
 	if (cooldown > 0.0){
 		$("#finalcooldown").prop("innerHTML", cooldown + "s");
 	} else {
