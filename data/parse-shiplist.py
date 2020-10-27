089986590b501394ebe3f69b4e26d31beb3c3ff0
Leo Izen <leo.izen@gmail.com>
2020-10-27 11:16:04 -0400

    parse-shiplist: use builtin open function

--- data/parse-shiplist.py
+++ data/parse-shiplist.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 
 import json
-import io
 import os
 
-jsonfile = io.open("shiplist.json", "r", encoding="UTF-8")
+jsonfile = open("shiplist.json", "r", encoding="UTF-8")
 jsonlist = json.load(jsonfile)
 jsonfile.close()
-jsonfile = io.open("shiplist.json", "w", encoding="UTF-8")
+jsonfile = open("shiplist.json", "w", encoding="UTF-8")
 json.dump(jsonlist, jsonfile, sort_keys=True, indent=4)
 jsonfile.close()
 
@@ -24,11 +23,11 @@ for entry in jsonlist["cargoquery"]:
 		os.symlink("../by-id/" + shipID, "ships/by-name/" + shipName, target_is_directory=True)
 	except FileExistsError:
 		pass
-	jsonfile = io.open(shipdir + "/ship.json", "w", encoding="UTF-8")
+	jsonfile = open(shipdir + "/ship.json", "w", encoding="UTF-8")
 	json.dump(ship, jsonfile, sort_keys=True, indent=4)
 	jsonfile.close()
 	toc["ships"].append({"ShipID":shipID, "Name":shipName, "dataJSON":(shipdir + "/ship.json")})
 
-jsonfile = io.open("ships/toc.json", "w", encoding="UTF-8")
+jsonfile = open("ships/toc.json", "w", encoding="UTF-8")
 json.dump(toc, jsonfile, sort_keys=True, indent=4)
 jsonfile.close()
