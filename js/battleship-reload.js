437285a72d1f5bc79a2d2a1c851ae87b841a0ee2
Leo Izen <leo.izen@gmail.com>
2020-11-01 08:42:05 -0500

    battleship-reload: Add more timers and init reduction

--- js/battleship-reload.js
+++ js/battleship-reload.js
@@ -2,10 +2,12 @@ function get_bbspeed(weapon_cooldown, reload_stat, reload_percent_buff){
 	return Math.sqrt(1.0 + reload_stat / 100.0 * (1 + reload_percent_buff / 100.0)) * Math.SQRT1_2 / weapon_cooldown; 
 }
 
-function get_bbcooldown(weapon_cooldown, reload_stat, reload_percent_buff, has_timed_reload_buff, timed_reload_percent_buff, timed_reload_buff_duration, cooldown_reduction_percent, has_boomer_fcr){
+function get_bbcooldown(weapon_cooldown, reload_stat, reload_percent_buff, has_timed_reload_buff, timed_reload_percent_buff, timed_reload_buff_duration, cooldown_reduction_percent, init_cooldown_reduction_percent, has_boomer_fcr){
 	let cooldown;
+	let init_cooldown;
+	init_cooldown = 1.0 / get_bbspeed(weapon_cooldown, reload_stat, reload_percent_buff);
 	if (!has_timed_reload_buff || timed_reload_buff_duration <= 0 || timed_reload_percent_buff <= 0){
-		cooldown = 1.0 / get_bbspeed(weapon_cooldown, reload_stat, reload_percent_buff);
+		cooldown = init_cooldown;
 	} else {
 		let speed_with_timed_buff = get_bbspeed(weapon_cooldown, reload_stat, reload_percent_buff + timed_reload_percent_buff);
 		let completion_with_buff = speed_with_timed_buff * timed_reload_buff_duration; 
@@ -17,14 +19,24 @@ function get_bbcooldown(weapon_cooldown, reload_stat, reload_percent_buff, has_t
 		}
 	}
 	let cd_reduction = +cooldown_reduction_percent;
+	let init_cd_reduction = +init_cooldown_reduction_percent;
+	init_cd_reduction += cd_reduction;
 	if (has_boomer_fcr){
-		cd_reduction += 15.0;
+		init_cd_reduction += 15.0;
 	}
+	init_cooldown = init_cooldown * (1.0 - init_cd_reduction / 100.0);
 	cooldown = cooldown * (1.0 - cd_reduction / 100.0);
-	if (cooldown > 0.0 && cooldown < 300.0){
-		return Math.round(cooldown * 100.0) / 100.0;
+
+	if (cooldown > 0.0 && cooldown < 300.0 && init_cooldown > 0.0 && init_cooldown < 300.00){
+		let timer = init_cooldown;
+		let ret = [Math.round(cooldown * 100.00) / 100.00];
+		while (timer < 300.00){
+			ret.push(Math.round(timer * 100.00) / 100.00);
+			timer += cooldown;
+		}
+		return ret;
 	} else {
-		return -1.0;
+		return [-1.0];
 	}
 }
 
@@ -44,15 +56,20 @@ function calculate_bbcooldown() {
 	let reloadbuff = $("#bbreloadbufftextfield").prop('value');
 	let weaponcd = $("#maingun1cdtextfield").prop('value');
 	let cooldown_reduction = $("#cdreduction1textfield").prop("value");
+	let initial_cd_reduction = $("#cdreduction2textfield").prop("value");
 	let has_timed_reload_buff = $('#cdred2enablecb').is(':checked');
 	let has_boomer_fcr = $("#boomerfcrbox").is(":checked");
 	let timed_reload_percent_buff = $('#cdred2percenttxtfield').prop('value');
 	let timed_reload_buff_duration = $('#cdred2timetxtfield').prop('value');
-	let cooldown = get_bbcooldown(+weaponcd, +reloadstat, +reloadbuff, has_timed_reload_buff, +timed_reload_percent_buff, +timed_reload_buff_duration, +cooldown_reduction, has_boomer_fcr);
-	if (cooldown > 0.0){
-		$("#finalbbcooldown").prop("innerHTML", cooldown + "s");
+	let cooldown = get_bbcooldown(+weaponcd, +reloadstat, +reloadbuff, has_timed_reload_buff, +timed_reload_percent_buff, +timed_reload_buff_duration, +cooldown_reduction, +initial_cd_reduction, has_boomer_fcr);
+	if (cooldown[0] > 0.0){
+		$("#finalbbcooldown").prop("innerHTML", cooldown.shift() + "s");
+		$("#initbbcooldown").prop("innerHTML", cooldown[0] + "s");
+		$("#finalbbshottimers").prop("innerHTML", cooldown.join(", ") + "");
 	} else {
 		$("#finalbbcooldown").prop("innerHTML", "Some Error Occurred :(");
+		$("#initbbcooldown").prop("innerHTML", "");
+		$("#finalbbshottimers").prop("innerHTML", "");
 	}
 }
 
