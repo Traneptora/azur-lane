c7efe4958b8f892d117107d886317543e8b21cbe
Leo Izen <leo.izen@gmail.com>
2020-12-16 11:44:23 -0500

    parse-shiplist.py: tabs to four spaces

--- data/parse-shiplist.py
+++ data/parse-shiplist.py
@@ -13,20 +13,20 @@ jsonfile.close()
 toc = {"ships":[]}
 
 for entry in jsonlist["cargoquery"]:
-	ship = entry["title"]
-	shipID = ship["ShipID"]
-	shipName = ship["Name"]
-	shipdir = "ships/by-id/" + shipID;
-	os.makedirs(shipdir, mode=0o755, exist_ok=True)
-	os.makedirs("ships/by-name/", mode=0o755, exist_ok=True)
-	try:
-		os.symlink("../by-id/" + shipID, "ships/by-name/" + shipName, target_is_directory=True)
-	except FileExistsError:
-		pass
-	jsonfile = open(shipdir + "/ship.json", "w", encoding="UTF-8")
-	json.dump(ship, jsonfile, sort_keys=True, indent=4)
-	jsonfile.close()
-	toc["ships"].append({"ShipID":shipID, "Name":shipName, "dataJSON":(shipdir + "/ship.json")})
+    ship = entry["title"]
+    shipID = ship["ShipID"]
+    shipName = ship["Name"]
+    shipdir = "ships/by-id/" + shipID;
+    os.makedirs(shipdir, mode=0o755, exist_ok=True)
+    os.makedirs("ships/by-name/", mode=0o755, exist_ok=True)
+    try:
+        os.symlink("../by-id/" + shipID, "ships/by-name/" + shipName, target_is_directory=True)
+    except FileExistsError:
+        pass
+    jsonfile = open(shipdir + "/ship.json", "w", encoding="UTF-8")
+    json.dump(ship, jsonfile, sort_keys=True, indent=4)
+    jsonfile.close()
+    toc["ships"].append({"ShipID":shipID, "Name":shipName, "dataJSON":(shipdir + "/ship.json")})
 
 jsonfile = open("ships/toc.json", "w", encoding="UTF-8")
 json.dump(toc, jsonfile, sort_keys=True, indent=4)
