962ef037290cc1e52663a36e5088e06f58085c74
Leo Izen <leo.izen@gmail.com>
2021-08-27 17:02:48 -0400

    battleship-reload: use common code from common.js

--- js/battleship-reload.js
+++ js/battleship-reload.js
@@ -31,9 +31,9 @@ function get_bbcooldown(weapon_cooldown, rld_stat_base, rld_bonus, reload_percen
 
     if (cooldown > 1.0 && cooldown < 300.0 && init_cooldown > 0.0 && init_cooldown < 300.00){
         let timer = init_cooldown;
-        let ret = [Math.round(cooldown * 100.00) / 100.00];
+        let ret = [roundBase10(cooldown, 2)];
         while (timer < 300.00){
-            ret.push(Math.round(timer * 100.00) / 100.00);
+            ret.push(roundBase10(timer, 2));
             timer += cooldown;
         }
         return ret;
@@ -63,9 +63,9 @@ function update_guntextfields() {
     img.alt = name;
 }
 
-function calculate_bbcooldown() {
-    const rld_stat = document.getElementById("bb-rld-stat-txt").value;
-    const rld_bonus = document.getElementById("bb-rld-bonus-txt").value;
+function calculate_reload() {
+    const rld_stat = document.getElementById("txt-rld-stat-base").value;
+    const rld_bonus = document.getElementById("txt-rld-stat-bonus").value;
     const reloadbuff = document.getElementById("bb-rld-buff-txt").value;
     const weaponcd = document.getElementById("bb-mg-1-txt").value;
     const cooldown_reduction = document.getElementById("bb-cd-red-txt-1").value;
@@ -86,59 +86,21 @@ function calculate_bbcooldown() {
     }
 }
 
-var battleship_list = [];
-var battleships = {};
-
-function handle_toc(data){
-    if (battleship_list.length > 0){
-        return;
-    }
-    for (let ship of data.ships){
-        let battleship_name = ship.Name;
-        battleship_list.push(battleship_name);
-        battleships[battleship_name] = ship;
-    }
-    battleship_list.sort();
-    battleship_list.push("Other");
-    for (let battleship_name of battleship_list){
-        let option = document.createElement("option");
-        option.name = battleship_name;
-        option.value = battleship_name;
-        option.appendChild(document.createTextNode(battleship_name));
-        document.getElementById("shipselect").appendChild(option);
-    }
-    document.querySelector("#shipselect > option[value=Georgia]").selected = true;
-    acquire_loadout();
+function get_fetch_url_impl(data){
+    return "/azur-lane/data/" + data.battleshipJSON;
 }
 
-function acquire_loadout(){
-    let battleship_name = document.getElementById("shipselect").value;
-    if (battleship_name === "Other"){
-        let other_obj = {"Name":"Other","Reload":100};
-        handle_loadout_data(other_obj);
-    } else {
-        const url = "/azur-lane/data/" + battleships[battleship_name].battleshipJSON;
-        fetch(url).then((response) => {
-            return response.json();
-        }).then((json) => {
-            handle_loadout_data(json);
-        });
-    }
-}
-
-function handle_loadout_data(data){
-    const rld_stat_base = +data.Reload;
-    document.getElementById("bb-rld-stat-txt").value = rld_stat_base;
-    calculate_bbcooldown();
+function handle_loadout_data_impl(data){
+    // pass
 }
 
 function ready() {
     update_guntextfields();
-    calculate_bbcooldown();
+    calculate_reload();
     fetch("/azur-lane/data/ships/battleships.json").then((r) => {
         return r.json();
     }).then((j) => {
-        handle_toc(j);
+        handle_toc(j, "Georgia");
     });
     document.querySelectorAll("#bb-mg-1-dropdown option").forEach((elem) => {
         const src = elem.dataset.imgname;
@@ -148,8 +110,4 @@ function ready() {
     });
 }
 
-if (document.readyState === 'loading'){
-    document.addEventListener("DOMContentLoaded", ready);
-} else {
-    ready();
-}
+document.addEventListener("DOMContentLoaded", ready);
